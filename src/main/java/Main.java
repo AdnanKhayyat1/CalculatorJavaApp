477443cf295591f6bde03b1ba0686fd21db5ac91
Adnan Khayyat <adnan-car@windowslive.com>
2021-02-27 20:45:58 -0700

    finally fixes input bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,22 +11,16 @@ public class Main {
         while(input != "done"){
             input = scanner.nextLine();
             String[] cmds = input.split("\\W+");
-            for(int i = 0; i < cmds.length; i++){
-                System.out.println(i + ": " + cmds[i] + ".");
-            }
             int leftOp = Integer.parseInt(cmds[1]);
             if(cmds[0].contains("fibonacciNumberFinder") || cmds[0].contains("intToBinaryNumber")){
-                System.out.println("one operand ops");
                 if(cmds[0] == "fibonacciNumberFinder"){
                     System.out.println(calc.fibonacciNumberFinder(leftOp));
                 }
-                
                 else if(cmds[0].contains("intToBinaryNumber")){
                     System.out.println("converting int to binary...");
                     System.out.println(calc.intToBinaryNumber(leftOp));
                 } 
             }else {
-                System.out.println("two operand ops");
                 int result = -1;
                 int rightOp = Integer.parseInt(cmds[2]);
                 if(cmds[0].contains("add")){
