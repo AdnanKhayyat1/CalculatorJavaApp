2cc81cfbee436165025474a6d7a4a73a6e6b7d21
Adnan Khayyat <adnan-car@windowslive.com>
2021-02-27 20:43:11 -0700

    input fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,13 +15,13 @@ public class Main {
                 System.out.println(i + ": " + cmds[i] + ".");
             }
             int leftOp = Integer.parseInt(cmds[1]);
-            if(cmds[0] == "fibonacciNumberFinder" || cmds[0] == "intToBinaryNumber"){
+            if(cmds[0].contains("fibonacciNumberFinder") || cmds[0].contains("intToBinaryNumber")){
                 System.out.println("one operand ops");
                 if(cmds[0] == "fibonacciNumberFinder"){
                     System.out.println(calc.fibonacciNumberFinder(leftOp));
                 }
                 
-                else if(cmds[0] == "intToBinaryNumber"){
+                else if(cmds[0].contains("intToBinaryNumber")){
                     System.out.println("converting int to binary...");
                     System.out.println(calc.intToBinaryNumber(leftOp));
                 } 
@@ -29,17 +29,17 @@ public class Main {
                 System.out.println("two operand ops");
                 int result = -1;
                 int rightOp = Integer.parseInt(cmds[2]);
-                if(cmds[0] == "add"){
+                if(cmds[0].contains("add")){
                     System.out.println("adding...");
                     result = calc.add(leftOp, rightOp);
                 }
-                if(cmds[0] == "subtract"){
+                if(cmds[0].contains("subtract")){
                     result = calc.add(leftOp, rightOp);
                 }
-                if(cmds[0] == "multiply"){
+                if(cmds[0].contains("multiply")){
                     result = calc.multiply(leftOp, rightOp);
                 }
-                if(cmds[0] == "divide"){
+                if(cmds[0].contains("divide")){
                     result = calc.divide(leftOp, rightOp);
                 }
                 System.out.println("Output: " + Integer.toString(result) + "\n");
